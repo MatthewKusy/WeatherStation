69f01187240567686e1686b1c24102211f44216b
MatthewKusy <mattk.player@gmail.com>
2026-06-09 13:48:26 -0700

    Added Dashboard Page and Updated Flask App to Serve Static Files

--- Server/app.py
+++ Server/app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="../Dashboard", static_url_path="")
 
-@app.route("/")
-def home():
-    return "Hello, Flask!"
+@app.get("/")
+def index():
+    return app.send_static_file("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
